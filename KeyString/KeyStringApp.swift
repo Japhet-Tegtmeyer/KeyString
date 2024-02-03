77b2c1cff525210cf066639bda9d3a2bef14db04
Japhet Tegtmeyer <japhteg@gmail.com>
2024-02-03 10:40:08 -0500

    Initial Commit

---
+++ KeyString/KeyStringApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyStringApp.swift
+//  KeyString
+//
+//  Created by Japhet Tegtmeyer on 2/3/24.
+//
+
+import SwiftUI
+
+@main
+struct KeyStringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
